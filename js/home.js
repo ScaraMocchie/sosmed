1c7e22b025bad0f0c9ad2a4552dce3a8d688f045
hazellenuts <140362133+hazellenuts@users.noreply.github.com>
2023-12-27 18:21:07 +0700

    updated home

--- js/home.js
+++ js/home.js
@@ -6,3 +6,38 @@ const logout =  () => {
       console.error('Logout Error:', error);
     });
   }
+
+  function adjustInputHeight(textarea) {
+    textarea.style.height = "auto";
+    textarea.style.height = (textarea.scrollHeight) + "px";
+}
+
+function insertImage() {
+    var imageInput = document.getElementById("imageInput");
+    var postInput = document.getElementById("postInput");
+    var selectedImageContainer = document.getElementById("selectedImageContainer");
+
+    var file = imageInput.files[0];
+    if (file) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            var imageTag = '<img src="' + e.target.result + '" alt="uploaded-image">';
+            selectedImageContainer.innerHTML = imageTag;
+        };
+        reader.readAsDataURL(file);
+        imageInput.value = '';
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var postInput = document.getElementById("postInput");
+  var imageContentInput = document.getElementById("imageContentInput");
+
+  postInput.addEventListener("click", function () {
+      imageContentInput.classList.add("active");
+  });
+
+  postInput.addEventListener("blur", function () {
+      imageContentInput.classList.remove("active");
+  });
+});
