b6d2d0956708540baa00c255c1cd7c18b8feefd7
nanadj33 <nadasyifadj33@gmail.com>
2023-12-28 12:13:44 +0700

    firebase fixed view

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,11 @@
 // Import the functions you need from the SDKs you need
-//   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-//   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyBS_z-3tlJR2SWxNJgbP1iA1iO7rL-9Cd4",
     authDomain: "socialmedia-bce32.firebaseapp.com",
